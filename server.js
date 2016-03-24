c5bbc62d4f63997f028d32b297e60f5c85bf07d5
rga78 <robertgalderman@gmail.com>
2016-03-24 17:33:16 -0600

    user-specific collections

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ passport.use(new BasicStrategy( function(username, password, done) {
 
                                     console.log("BasicStrategy.callback: username=" + username + ", password hash:" + hash);
 
-                                    var collection = db.get('tm-users');
+                                    var collection = db.get('/tm/users');
                                     collection.id = function (id) { return id; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
                                     collection.findOne({ "_id": username,
                                                          "password": hash 
                                                        },
                                                        { },
                                                        function(err,user){
-                                                           console.log("BasicStrategy:tm-users.find: err=" + JSON.stringify(err) 
+                                                           console.log("BasicStrategy:/tm/users.find: err=" + JSON.stringify(err) 
                                                                                                   + ", user=" + JSON.stringify(user) );
                                                            if (err) {
                                                                return done(err);
@@ -78,7 +78,11 @@ app.use( passport.initialize() );
 
 
 /**
- * This is like a servlet filter.
+ * http://expressjs.com/en/guide/using-middleware.html
+ * app.use() registers MIDDLEWARE functions.
+ * Each middleware function must call next() to invoke the next middleware function.
+ * Very similar to servlet filters.
+ *
  * Append db to the req object so its accessible to our routes.
  */
 app.use(function(req,res,next){
@@ -88,11 +92,34 @@ app.use(function(req,res,next){
 
 
 /**
+ * http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+ */
+var mongoIdFn = function(id) {
+    return id;
+};
+
+/**
+ * @param req object, which should have req.db and req.user attached.
+ * @param collectionName
+ *
+ * @return the user's collection, e.g. "/tm/{user._id}/{collectionName}"
+ */
+var getUserCollection = function(req, collectionName) {
+    var retMe = req.db.get("/tm/" + req.user._id + "/" + collectionName);
+    retMe.id = mongoIdFn;  
+    return retMe;
+};
+
+
+
+/**
  * REST API
  * @return all acounts.
  */
-app.get('/accounts', function (req, res) {
-    var collection = req.db.get('accounts');
+app.get('/accounts', 
+        passport.authenticate('basic', { session: false }),
+        function (req, res) {
+    var collection = getUserCollection(req, "accounts");
     collection.find({},
                     {},
                     function(err,docs){
@@ -106,8 +133,9 @@ app.get('/accounts', function (req, res) {
  * @return all tags.
  */
 app.get('/tags', 
+        passport.authenticate('basic', { session: false }),
         function (req, res) {
-    var collection = req.db.get('tags');
+    var collection = getUserCollection(req, "tags");
     collection.findOne({ "_id": 1 },
                        {},
                        function(err,doc){
@@ -124,11 +152,9 @@ app.get('/tags',
 app.get('/accounts/:id', 
         passport.authenticate('basic', { session: false }),
         function (req, res) {
-    var collection = req.db.get('accounts');
     console.log("GET /accounts/" + req.params.id);
 
-    collection.id = function (id) { return id; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
-
+    var collection = getUserCollection(req, "accounts");
     collection.findOne({ "_id": parseInt(req.params.id) },
                        {},
                        function(err,doc){
@@ -148,8 +174,7 @@ app.get('/accounts/:id/timeseries',
         function (req, res) {
     console.log("GET /accounts/" + req.params.id + "/timeseries");
 
-    var collection = req.db.get('accountsTimeSeries');
-    collection.id = function (id) { return id; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+    var collection = getUserCollection(req, "accountsTimeSeries");
 
     collection.find({ "accountId": parseInt(req.params.id) },
                     { "sort": { "timestamp": 1 } },
@@ -162,14 +187,12 @@ app.get('/accounts/:id/timeseries',
 });
 
 
-
 /**
  * Fetch the object with the given id in the given collection.
  * Pass it to the callback.
  */
 var fetchById = function( collection, id, callback ) {
     console.log("fetchById: collection=TODO, id=" + id);
-    collection.id = function (id) { return id; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
     collection.findOne({ "_id": id }, {}, callback );
 };
 
@@ -178,7 +201,7 @@ var fetchById = function( collection, id, callback ) {
  * Fetch all trans in the given account.  
  * Passed to the callback.
  */
-var fetchAccountTrans = function( db, account, callback ) {
+var fetchAccountTrans = function( tranCollection, account, callback ) {
     if (account == null) {
         callback(null, []);
         return;
@@ -186,16 +209,15 @@ var fetchAccountTrans = function( db, account, callback ) {
 
     console.log("fetchAccountTrans: account.accountName=" + account.accountName);
 
-    var collection = db.get("transactions")
-    collection.find({ 
-                        "account": account.accountName,
-                        "fi": account.fiName,
-                        "isResolved": { "$exists": false }
-                    },
-                    { 
-                        "sort": { "timestamp": -1 } 
-                    },
-                    callback );
+    tranCollection.find({ 
+                            "account": account.accountName,
+                            "fi": account.fiName,
+                            "isResolved": { "$exists": false }
+                        },
+                        { 
+                            "sort": { "timestamp": -1 } 
+                        },
+                        callback );
 };
 
 
@@ -208,13 +230,15 @@ app.get('/accounts/:id/transactions',
         function (req, res) {
     console.log("GET /accounts/" + req.params.id + "/transactions");
 
-    fetchById( req.db.get("accounts"), 
+    fetchById( getUserCollection(req, "accounts"),
                parseInt(req.params.id), 
                function(e, account) {
-                   fetchAccountTrans(req.db, account, function(e, trans) {
-                       console.log("GET /accounts/" + req.params.id + "/transactions: RESPONSE: trans.length=" + trans.length );
-                       res.json(trans);
-                   });
+                   fetchAccountTrans(getUserCollection(req, "transactions"), 
+                                     account, 
+                                     function(e, trans) {
+                                         console.log("GET /accounts/" + req.params.id + "/transactions: RESPONSE: trans.length=" + trans.length );
+                                         res.json(trans);
+                                     });
                });
 });
 
@@ -226,10 +250,9 @@ app.get('/accounts/:id/transactions',
 app.put('/accounts/:id', 
         passport.authenticate('basic', { session: false }),
         function (req, res) {
-    var collection = req.db.get('accounts');
     console.log("PUT /accounts/" + req.params.id + ": REQUEST: " + JSON.stringify(req.body))
 
-    collection.id = function (str) { return str; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+    var collection = getUserCollection(req, "accounts");
 
     // TODO: we should NEVER update mint fields... perhaps we should strip them out before updating.
     //       and for that matter, we shoudl never "upsert".
@@ -252,10 +275,9 @@ app.put('/accounts/:id',
 app.get('/transactions/:id', 
         passport.authenticate('basic', { session: false }),
         function (req, res) {
-    var collection = req.db.get('transactions');
     console.log("GET /transactions/" + req.params.id);
 
-    collection.id = function (id) { return id; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+    var collection = getUserCollection(req, "transactions");
 
     collection.find({ "_id": parseInt(req.params.id) },
                     {},
@@ -275,10 +297,9 @@ app.get('/transactions/:id',
 app.put('/transactions/:id', 
         passport.authenticate('basic', { session: false }),
         function (req, res) {
-    var collection = req.db.get('transactions');
     console.log("PUT /transactions/" + req.params.id + ": REQUEST: " + JSON.stringify(req.body))
 
-    collection.id = function (str) { return str; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+    var collection = getUserCollection(req, "transactions");
 
     // TODO: we should NEVER update mint fields... perhaps we should strip them out before updating.
     //       and for that matter, we shoudl never "upsert".
@@ -293,8 +314,8 @@ app.put('/transactions/:id',
                       });
 
     // Also update the tags collection
-    var tagsCollection = req.db.get('tags');
-    tagsCollection.id = function (str) { return str; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+
+    var tagsCollection = getUserCollection(req, "tags");
 
     tagsCollection.update({ '_id': 1 }, 
                           { '$addToSet': { 'tags': { '$each': (req.body.tags || []) } } } ,
@@ -319,8 +340,11 @@ app.put('/transactions/:id',
 app.post('/query/accounts', 
          passport.authenticate('basic', { session: false }),
          function (req, res) {
-    var collection = req.db.get('accounts');
+
     console.log("POST /query/accounts: REQUEST: " + JSON.stringify(req.body))
+    console.log("POST /query/accounts: user: " + JSON.stringify(req.user));
+
+    var collection = getUserCollection(req, "accounts");
     collection.find( req.body.query || {}, 
                      req.body.options || {},
                      function(err,docs){
@@ -344,8 +368,8 @@ app.post('/query/accounts',
 app.post('/query/transactions', 
          passport.authenticate('basic', { session: false }),
          function (req, res) {
-    var collection = req.db.get('transactions');
     console.log("POST transactions/query: REQUEST: " + JSON.stringify(req.body))
+    var collection = getUserCollection(req, "transactions");
     collection.find( req.body.query || {}, 
                      req.body.options || {},
                      function(err,docs){
@@ -370,8 +394,9 @@ app.post('/query/transactions',
 app.post('/query/transactions/count', 
          passport.authenticate('basic', { session: false }),
          function (req, res) {
-    var collection = req.db.get('transactions');
     console.log("POST transactions/query/count: REQUEST: " + JSON.stringify(req.body))
+
+    var collection = getUserCollection(req, "transactions");
     collection.count( req.body.query || {}, 
                      function(err,count){
                          console.log("POST transactions/query/count: " + count
@@ -393,8 +418,9 @@ app.post('/query/transactions/count',
 app.post('/query/transactions/summary', 
          passport.authenticate('basic', { session: false }),
          function (req, res) {
-    var collection = req.db.get('transactions');
     console.log("POST transactions/query/summary: REQUEST: " + JSON.stringify(req.body))
+
+    var collection = getUserCollection(req, "transactions");
     collection.find( req.body.query || {}, 
                      function(err,trans){
                          trans = trans || [];
@@ -412,7 +438,6 @@ app.post('/query/transactions/summary',
 });
 
 
-
 /**
  * REST API
  * @return WriteResult
@@ -420,14 +445,14 @@ app.post('/query/transactions/summary',
 app.post('/savedqueries', 
          passport.authenticate('basic', { session: false }),
          function (req, res) {
-    var collection = req.db.get('savedqueries');
     console.log("POST /savedqueries: REQUEST: " + JSON.stringify(req.body))
 
     var savedQuery = req.body;
     savedQuery.query = JSON.stringify( savedQuery.query );
     console.log("POST /savedqueries: savedQuery: " + JSON.stringify(savedQuery))
 
-    collection.id = function (str) { return str; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+    var collection = getUserCollection(req, "savedqueries");
+
     collection.update({ "_id": savedQuery.name }, 
                       { "$set": savedQuery },
                       { "upsert": true },
@@ -446,7 +471,8 @@ app.post('/savedqueries',
 app.get('/savedqueries', 
         passport.authenticate('basic', { session: false }),
         function (req, res) {
-    var collection = req.db.get('savedqueries');
+
+    var collection = getUserCollection(req, "savedqueries");
     collection.find({},
                     {},
                     function(err,savedQueries) {
@@ -471,8 +497,7 @@ app.get('/savedqueries/:id',
 
     console.log("GET /savedqueries/" + req.params.id);
 
-    var collection = req.db.get('savedqueries');
-    collection.id = function (id) { return id; };  // http://stackoverflow.com/questions/25889863/play-with-meteor-mongoid-in-monk
+    var collection = getUserCollection(req, "savedqueries");
 
     collection.findOne({ "_id": req.params.id },
                        {},
@@ -488,6 +513,9 @@ app.get('/savedqueries/:id',
 
 
 
+/**
+ * app.listen initiates the server.
+ */
 var server = app.listen(process.env.PORT || 8081, function () {
 
   var host = server.address().address
